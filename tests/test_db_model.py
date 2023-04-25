47470e1b8d71eae5295df04101d9c0df4f3f3f2a
weijay <weijay0804@gmail.com>
2023-04-25 16:13:17 +0800

    test: add restaurant table CRUD function unit test
    新增針對 restaurant table 的 CRUD 功能測試

--- tests/test_db_model.py
+++ tests/test_db_model.py
@@ -2,7 +2,7 @@
 Author: weijay
 Date: 2023-04-24 23:09:47
 LastEditors: weijay
-LastEditTime: 2023-04-25 00:09:13
+LastEditTime: 2023-04-25 16:07:03
 Description: DataBase ORM 模型單元測試
 '''
 
@@ -12,7 +12,9 @@ from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app.schemas import restaurant_scheme
 from app.model import Restaurant, Base
+from app import crud
 
 
 class InitialDataBaseTest(unittest.TestCase):
@@ -122,3 +124,77 @@ class TestRestaurantModel(InitialDataBaseTest):
         restaurant = self.db.query(Restaurant).filter(Restaurant.name == "測試4").first()
 
         self.assertIsNone(restaurant)
+
+
+class TestRestaurantCURD(InitialDataBaseTest):
+    def setUp(self) -> None:
+        self.db = self.SessionLocal()
+
+        self.fake_restaurant = Restaurant(
+            name="測試1",
+            address="台北市信義區松壽路6段",
+            lat=25.03586,
+            lng=121.56433,
+            phone="02-1284193212",
+        )
+
+        self.db.add(self.fake_restaurant)
+        self.db.commit()
+
+    def tearDown(self) -> None:
+        self.db.delete(self.fake_restaurant)
+        self.db.commit()
+        self.db.close()
+
+    def test_get_restaurants_function(self):
+        restaurants = crud.get_restaurants(self.db)
+
+        self.assertTrue(isinstance(restaurants, list))
+        self.assertTrue(isinstance(restaurants[0], Restaurant))
+
+    def test_create_restaurant_function(self):
+        restaurant = restaurant_scheme.ResCreateModel(
+            name="測試2", address="新北市汐止區大同路一段", phone="02-12334553"
+        )
+
+        db_restaurant = crud.create_restaurant(self.db, restaurant)
+
+        self.assertIsNotNone(db_restaurant)
+        self.assertTrue(isinstance(db_restaurant, Restaurant))
+        self.assertEqual(db_restaurant.name, "測試2")
+
+    def test_update_restaurant_function(self):
+        restaurant = self.db.query(Restaurant).filter(Restaurant.name == "測試2").first()
+
+        update_data = restaurant_scheme.ResCreateModel(name="測試2更新", address=restaurant.address)
+
+        updated_restaurant = crud.update_restaurant(self.db, restaurant.id, update_data)
+
+        self.assertIsNotNone(updated_restaurant)
+        self.assertEqual(updated_restaurant.name, "測試2更新")
+        self.assertEqual(updated_restaurant.address, "新北市汐止區大同路一段")
+        self.assertEqual(updated_restaurant.phone, "02-12334553")
+
+    def test_update_restaurant_function_with_not_exist_id(self):
+        update_data = restaurant_scheme.ResCreateModel(name="not exist", address="not exist")
+
+        upadted_restaurant = crud.update_restaurant(self.db, 1000, update_data)
+
+        self.assertIsNone(upadted_restaurant)
+
+    def test_delete_restaurant_function(self):
+        restaurant = self.db.query(Restaurant).filter(Restaurant.name == "測試1").first()
+
+        crud.delete_restaurant(self.db, restaurant.id)
+
+        deleted_restaurant = (
+            self.db.query(Restaurant).filter(Restaurant.id == restaurant.id).first()
+        )
+
+        self.assertIsNotNone(restaurant)
+        self.assertIsNone(deleted_restaurant)
+
+    def test_delete_restaurant_function_with_not_eixst_id(self):
+        deleted_restaurant = crud.delete_restaurant(self.db, 1000)
+
+        self.assertIsNone(deleted_restaurant)
