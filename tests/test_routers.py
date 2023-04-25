e9d6667a4f47bd4c23390ae986fe19ac0fc67818
weijay <weijay0804@gmail.com>
2023-04-26 00:02:10 +0800

    test: add restaurant router unit test
    新增 餐廳路由功能單元測試

---
+++ tests/test_routers.py
@@ -0,0 +1,95 @@
+'''
+Author: weijay
+Date: 2023-04-25 16:26:37
+LastEditors: weijay
+LastEditTime: 2023-04-25 23:55:44
+Description: Api Router 單元測試
+'''
+
+
+import os
+import unittest
+
+from fastapi.testclient import TestClient
+
+from app.config import config
+from app import create_app
+from app.model import Restaurant
+from app.routers import register_router
+from tests.utils import FakeDataBase
+from app.routers.restaurant_router import get_db
+
+
+class InitialTestClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_app = create_app("test")
+
+        register_router(cls.test_app, f"/api/{config.TestConfig.API_VERSION}")
+
+        cls.fake_database = FakeDataBase()
+
+        cls.fake_database.Base.metadata.create_all(bind=cls.fake_database.engine)
+
+        cls.client = TestClient(cls.test_app)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.fake_database.engine.clear_compiled_cache()
+        cls.fake_database.engine.dispose()
+        cls.fake_database.Base.metadata.drop_all(bind=cls.fake_database.engine)
+        os.remove("test.db")
+
+
+class TestResaurantRotuer(InitialTestClient):
+    def setUp(self) -> None:
+        self.test_app.dependency_overrides[get_db] = self.fake_database.override_get_db
+
+    def test_read_restaurant_router(self):
+        restaurant = Restaurant(name="測試餐廳", address="測試地址", lat=23.0012, lng=123.0123)
+
+        with self.fake_database.get_db() as db:
+            db.add(restaurant)
+            db.commit()
+
+        response = self.client.get("/api/v1/restaurant")
+
+        data = response.json()["items"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(data, list))
+        self.assertIsNotNone(data[0]["name"])
+        self.assertIsNotNone(data[0]["address"])
+
+    def test_create_restaurant_router(self):
+        response = self.client.post(
+            "/api/v1/restaurant", json={"name": "測試餐廳2", "address": "測試地址2"}
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {"message": "created."})
+
+    def test_upate_restaurant_router(self):
+        with self.fake_database.get_db() as db:
+            restaurant = db.query(Restaurant).filter(Restaurant.name == "測試餐廳").first()
+
+        response = self.client.patch(
+            f"/api/v1/restaurant/{restaurant.id}",
+            json={"name": "測試餐廳更新", "address": restaurant.address},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["name"], "測試餐廳更新")
+        self.assertEqual(response.json()["address"], restaurant.address)
+
+    def test_delete_restaurant_router(self):
+        with self.fake_database.get_db() as db:
+            restaurant = db.query(Restaurant).filter(Restaurant.name == "測試餐廳2").first()
+
+        response = self.client.delete(f"/api/v1/restaurant/{restaurant.id}")
+
+        with self.fake_database.get_db() as db:
+            deleted_restaurant = db.query(Restaurant).filter(Restaurant.name == "測試餐廳2").first()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNone(deleted_restaurant)
---
+++ tests/utils.py
@@ -0,0 +1,46 @@
+'''
+Author: weijay
+Date: 2023-04-25 17:19:24
+LastEditors: weijay
+LastEditTime: 2023-04-26 00:00:07
+Description: 放一些測試時會用到的通用函示
+'''
+
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.model import Base
+
+
+class FakeDataBase:
+    """產生測試資料庫"""
+
+    SQLALCHEMY_DATABASE_URL = "sqlite:///test.db"
+
+    def __init__(self):
+        self.engine = create_engine(
+            self.SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+        )
+
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+        self.Base = Base
+
+    def override_get_db(self):
+        try:
+            db = self.SessionLocal()
+            yield db
+
+        finally:
+            db.close()
+
+    @contextmanager
+    def get_db(self):
+        try:
+            db = self.SessionLocal()
+            yield db
+
+        finally:
+            db.close()
