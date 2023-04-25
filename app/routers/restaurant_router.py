67e4f09e2fa6ff276cd323a55072afc5997735a3
weijay <weijay0804@gmail.com>
2023-04-25 23:41:34 +0800

    feat: add restaurant router

--- app/routers/restaurant_router.py
+++ app/routers/restaurant_router.py
@@ -2,11 +2,9 @@
 Author: weijay
 Date: 2023-04-24 15:58:18
 LastEditors: weijay
-LastEditTime: 2023-04-25 16:07:11
+LastEditTime: 2023-04-25 23:26:14
 Description: 餐廳路由
 '''
-
-from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -16,8 +14,6 @@ from app.database import SessionLocal
 from app import crud
 
 router = APIRouter(prefix="/restaurant")
-
-fake_data: List[ResReadModel] = []
 
 
 def get_db():
@@ -31,7 +27,9 @@ def get_db():
 
 
 @router.get("/", response_model=ResReadModel)
-def read_restaurant(db: Session = Depends(get_db)):
+def read_restaurants(db: Session = Depends(get_db)):
+    """取得所有餐廳"""
+
     items = crud.get_restaurants(db)
 
     return ResReadModel(items=items)
@@ -39,6 +37,8 @@ def read_restaurant(db: Session = Depends(get_db)):
 
 @router.post("/", status_code=201)
 def create_restaurant(items: ResCreateModel, db: Session = Depends(get_db)):
+    """新增餐廳"""
+
     crud.create_restaurant(db, items)
 
     return {"message": "created."}
@@ -46,6 +46,8 @@ def create_restaurant(items: ResCreateModel, db: Session = Depends(get_db)):
 
 @router.patch("/{restaurant_id}", response_model=ResModel)
 def update_restaurant(restaurant_id: str, item: ResCreateModel, db: Session = Depends(get_db)):
+    """更新餐廳"""
+
     updated_restaurant = crud.update_restaurant(db, restaurant_id, item)
 
     if not updated_restaurant:
@@ -59,6 +61,8 @@ def update_restaurant(restaurant_id: str, item: ResCreateModel, db: Session = De
 
 @router.delete("/{restaurant_id}", status_code=200)
 def delete_restaurant(restaurant_id: str, db: Session = Depends(get_db)):
+    """刪除餐廳"""
+
     deleted_restaurant = crud.delete_restaurant(db, restaurant_id)
 
     if not deleted_restaurant:
--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 Author: weijay
 Date: 2023-04-24 15:56:51
 LastEditors: weijay
-LastEditTime: 2023-04-25 22:03:36
+LastEditTime: 2023-04-25 22:05:43
 Description: server 主程式
 '''
 
 from app import create_app
 from app.routers import register_router
+from app.database import engine
+from app import model
+
+model.Base.metadata.create_all(bind=engine)
 
 app = create_app("dev")
 
