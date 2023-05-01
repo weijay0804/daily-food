714b59a7291ccd7883c5b5440642857151f7c7b3
weijay <weijay0804@gmail.com>
2023-05-01 16:55:56 +0800

    feat: remove app.utils.LatLng
    因為負責計算經緯度距離的邏輯已經移動到了 app.database.crud.get_restaurant_randomly 函示中了，所以移除掉，同時移除有關的 unit test

--- app/utils.py
+++ app/utils.py
@@ -9,59 +9,10 @@ Description: 放一些輔助通用的函示
 
 import os
 from typing import Tuple
-import math
 
 from dotenv import load_dotenv
 from requests.exceptions import ReadTimeout
 import requests
-
-
-class LatLng:
-    """經緯度轉換"""
-
-    R = 6371  # 地球半徑 (km)
-
-    def _to_radian(self, radius: float) -> float:
-        return radius * 180 / math.pi
-
-    def __init__(self, lat: float, lng: float):
-        self.lat = lat
-        self.lng = lng
-
-    def _haversine(self, dis: float) -> Tuple[float, float]:
-        """計算以經緯度為圓心， r為半徑的東西向半徑長度和南北向半徑長度
-
-        Args:
-            dis (int): 範圍 (km)
-
-        Returns:
-            Tuple[float, float]: [lat 方向半徑長度, lng 方向半徑長度]
-        """
-
-        # fmt:off
-        d_lng = (
-            2 * math.asin(math.sin(dis / (2 * self.R))) / math.cos(self.lat * math.pi / 180)
-        )
-        d_lng = self._to_radian(d_lng)
-
-        d_lat = dis / self.R
-        d_lat = self._to_radian(d_lat)
-
-        return (d_lat, d_lng)
-
-    def get_distance(self, dis: float) -> Tuple[float, float, float, float]:
-        """取得以自身經緯度為圓心，dis 為半徑，計算出的矩形的四個點
-
-        Args:
-            dis (float): 半徑 (km)
-
-        Returns:
-            Tuple[float, float, float, float]: (lat_min, lat_max, lng_min, lng_max)
-        """
-
-        d_lat, d_lng = self._haversine(dis)
-
-        return (self.lat - d_lat, self.lat + d_lat, self.lng - d_lng, self.lng + d_lng)
 
 
 class MapApi:
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 Author: weijay
 Date: 2023-04-26 01:40:39
 LastEditors: weijay
-LastEditTime: 2023-04-26 23:51:19
+LastEditTime: 2023-05-01 16:53:21
 Description: app.utils 單元測試
 '''
 
@@ -12,41 +12,7 @@ from unittest.mock import patch, MagicMock
 
 import requests
 
-from app.utils import LatLng, MapApi
-
-
-class TestLatLng(unittest.TestCase):
-    CASE = [
-        (24.94093, 121.21433),
-        (24.94112, 121.21438),
-        (24.94057, 121.21526),
-        (24.98239, 121.26795),
-        (25.00352, 121.16513),
-        (25.04174, 121.07653),
-    ]
-
-    def test_get_distance(self):
-        lat = 24.94097
-        lng = 121.21456
-
-        lat_min, lat_max, lng_min, lng_max = LatLng(lat, lng).get_distance(0.5)
-
-        r = []
-
-        for d in self.CASE:
-            d_lat, d_lng = d
-
-            if lat_min <= d_lat <= lat_max and lng_min <= d_lng <= lng_max:
-                r.append((d_lat, d_lng))
-
-        self.assertEqual(
-            r,
-            [
-                (24.94093, 121.21433),
-                (24.94112, 121.21438),
-                (24.94057, 121.21526),
-            ],
-        )
+from app.utils import MapApi
 
 
 class TestMapApi(unittest.TestCase):
