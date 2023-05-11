06919c5c4d10f8203eb3b3a4fc7897a8705fa30e
weijay <weijay0804@gmail.com>
2023-05-11 23:07:52 +0800

    test: add restaurant_open_time table ORM model unit test

--- tests/test_db_model.py
+++ tests/test_db_model.py
@@ -2,18 +2,18 @@
 Author: weijay
 Date: 2023-04-24 23:09:47
 LastEditors: weijay
-LastEditTime: 2023-05-05 02:00:14
+LastEditTime: 2023-05-11 23:03:09
 Description: DataBase ORM 模型單元測試
 '''
 
 import os
 import unittest
-from datetime import datetime
+from datetime import datetime, time
 
 from sqlalchemy import text
 
 from app.schemas import restaurant_schema
-from app.database.model import Restaurant
+from app.database.model import Restaurant, RestaurantOpenTime
 from app.database import crud
 from tests.utils import FakeData, FakeDataBase
 
@@ -130,6 +130,113 @@ class TestRestaurantModel(InitialDataBaseTest):
             restaurant = db.query(Restaurant).filter(Restaurant.name == fake_data["name"]).first()
 
         self.assertIsNone(restaurant)
+
+
+class TestRestaurantOpenTimeModel(InitialDataBaseTest):
+    """RestaurantOpenTime ORM 模型單元測試"""
+
+    def setUp(self) -> None:
+        # NOTE 如果 restaurant_open_time table 有更改的話，要檢查一下這邊
+        fake_restaurant_open_time = RestaurantOpenTime(
+            restaurant_id=1,
+            day_of_week=1,
+            open_time=time(hour=10, minute=0),
+            close_time=time(hour=20, minute=0),
+        )
+
+        with self.fake_database.get_db() as db:
+            db.add(fake_restaurant_open_time)
+            db.commit()
+
+    def tearDown(self) -> None:
+        with self.fake_database.get_db() as db:
+            db.execute(text("DELETE FROM restaurant_open_time"))
+            db.commit()
+
+    def test_create_restaurant_open_time(self):
+        fake_restaurant_open_time = FakeData.fake_restaurant_open_time()
+
+        with self.fake_database.get_db() as db:
+            db.add(RestaurantOpenTime(**fake_restaurant_open_time, restaurant_id=2))
+            db.commit()
+
+        with self.fake_database.get_db() as db:
+            data = db.query(RestaurantOpenTime).filter(RestaurantOpenTime.id == 2).first()
+
+        self.assertIsNotNone(data)
+        self.assertEqual(data.id, 2)
+        self.assertEqual(data.day_of_week, fake_restaurant_open_time["day_of_week"])
+        self.assertIsInstance(data.open_time, time)
+        self.assertEqual(data.open_time, fake_restaurant_open_time["open_time"])
+        self.assertIsInstance(data.close_time, time)
+        self.assertEqual(data.close_time, fake_restaurant_open_time["close_time"])
+        self.assertIsInstance(data.create_at, datetime)
+        self.assertIsNotNone(data.create_at)
+        self.assertIsNone(data.update_at)
+
+    def test_read_restaurant_open_time(self):
+        with self.fake_database.get_db() as db:
+            data = (
+                db.query(RestaurantOpenTime).filter(RestaurantOpenTime.restaurant_id == 1).first()
+            )
+
+        self.assertIsNotNone(data)
+        self.assertEqual(data.id, 1)
+        self.assertEqual(data.open_time, time(hour=10, minute=0))
+        self.assertEqual(data.close_time, time(hour=20, minute=0))
+
+    def test_update_restaurant_open_time(self):
+        fake_restaurant_open_time = FakeData.fake_restaurant_open_time()
+
+        with self.fake_database.get_db() as db:
+            db.add(RestaurantOpenTime(**fake_restaurant_open_time, restaurant_id=3))
+            db.commit()
+
+        with self.fake_database.get_db() as db:
+            data = (
+                db.query(RestaurantOpenTime).filter(RestaurantOpenTime.restaurant_id == 3).first()
+            )
+
+            self.assertIsNone(data.update_at)
+
+            data.open_time = time(hour=12, minute=45)
+            data.update_at = datetime.utcnow()
+
+            db.commit()
+
+        with self.fake_database.get_db() as db:
+            updated_data = (
+                db.query(RestaurantOpenTime).filter(RestaurantOpenTime.restaurant_id == 3).first()
+            )
+
+        self.assertEqual(updated_data.day_of_week, fake_restaurant_open_time["day_of_week"])
+        self.assertEqual(updated_data.open_time, time(hour=12, minute=45))
+        self.assertIsNotNone(updated_data.update_at)
+
+    def test_delete_restaurant_open_time(self):
+        fake_restaurant_open_time = FakeData.fake_restaurant_open_time()
+
+        with self.fake_database.get_db() as db:
+            db.add(RestaurantOpenTime(**fake_restaurant_open_time, restaurant_id=4))
+            db.commit()
+
+        with self.fake_database.get_db() as db:
+            data = (
+                db.query(RestaurantOpenTime).filter(RestaurantOpenTime.restaurant_id == 4).first()
+            )
+
+        self.assertIsNotNone(data)
+
+        with self.fake_database.get_db() as db:
+            db.delete(data)
+            db.commit()
+
+        with self.fake_database.get_db() as db:
+            deleted_data = (
+                db.query(RestaurantOpenTime).filter(RestaurantOpenTime.restaurant_id == 4).first()
+            )
+
+        self.assertIsNone(deleted_data)
 
 
 class TestRestaurantCURD(InitialDataBaseTest):
