cceb2f4bee873c323e3e26423978660b65055d20
weijay <weijay0804@gmail.com>
2023-05-16 14:54:02 +0800

    feat: 新增 restaurant_open_time table 的 schemas model

--- app/schemas/database_schema.py
+++ app/schemas/database_schema.py
@@ -2,9 +2,11 @@
 Author: weijay
 Date: 2023-05-15 20:03:52
 LastEditors: weijay
-LastEditTime: 2023-05-15 20:47:45
+LastEditTime: 2023-05-15 22:03:49
 Description: 針對 database 中的 table 定義的 schemas
 '''
+
+import datetime
 from pydantic import BaseModel
 from typing import Union
 
@@ -17,3 +19,11 @@ class RestaurantDBModel(BaseModel):
     phone: Union[str, None] = None
     lat: float
     lng: float
+
+
+class RestaurantOpenTimeDBModel(BaseModel):
+    """要建立 restaurant_open_time table 的資料時的 schemas model"""
+
+    day_of_week: int
+    open_time: datetime.time
+    close_time: datetime.time
