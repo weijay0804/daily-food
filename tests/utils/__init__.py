66d966970a5f010e70242e74babf272e5e3bb160
weijay <weijay0804@gmail.com>
2023-06-10 13:55:22 +0800

    feat: 新增 可以指定產生測試資料數量功能

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -2,7 +2,7 @@
 Author: weijay
 Date: 2023-04-25 17:19:24
 LastEditors: andy
-LastEditTime: 2023-06-09 19:11:43
+LastEditTime: 2023-06-10 13:18:38
 Description: 放一些測試時會用到的通用函示
 '''
 
@@ -20,7 +20,7 @@ from . import _fake_data
 class FakeData:
     """用來建立隨機測試資料"""
 
-    def fake_restaurant(is_lat_lng: bool = True):
+    def fake_restaurant(is_lat_lng: bool = True, number: int = 1):
         """生成假餐廳資料，比較近的
 
         這裡的比較近是指使用 `fake_current_location()` 生成目標位置
@@ -28,20 +28,45 @@ class FakeData:
 
         簡而言之，如果使用 `fake_restaurant()` 生成餐廳位置，如果要測試在目標範圍內的餐廳，就使用 `fake_current_location()` ( 5KM 內 )
         """
-        result = {}
 
-        name = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME)
-        address = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS)
+        if number <= 1:
+            result = {}
 
-        if is_lat_lng:
-            lat, lng = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG)
-            result.update({"lat": lat, "lng": lng})
+            name = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME)
+            address = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS)
 
-        result.update({"name": name, "address": address})
+            if is_lat_lng:
+                lat, lng = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG)
+                result.update({"lat": lat, "lng": lng})
+
+            result.update({"name": name, "address": address})
+
+        else:
+            result = []
+
+            name_list = random.sample(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME, number)
+            address_list = random.sample(
+                _fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS, number
+            )
+
+            for i in range(number):
+                tmp = {"name": name_list[i], "address": address_list[i]}
+
+                result.append(tmp)
+
+            if is_lat_lng:
+                lat_lng_list = random.sample(
+                    _fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG, number
+                )
+
+                for i in range(number):
+                    lat, lng = lat_lng_list[i]
+
+                    result[i].update({"lat": lat, "lng": lng})
 
         return result
 
-    def fake_restaurant_far(is_lat_lng: bool = True):
+    def fake_restaurant_far(is_lat_lng: bool = True, number: int = 1):
         """生成假餐廳資料，比較遠的
 
         這裡的比較遠是指使用 `fake_cureent_location()` 生成的目標位置
@@ -49,20 +74,47 @@ class FakeData:
 
         簡而言之，如果使用 `fake_restaurant_far()` 生成餐廳位置，如果要測試不在目標範圍內的餐廳，就用 `fake_current_location_far()`
         """
-        result = {}
 
-        name = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME_FAR)
-        address = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS_FAR)
+        if number <= 1:
+            result = {}
 
-        if is_lat_lng:
-            lat, lng = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG_FAR)
-            result.update({"lat": lat, "lng": lng})
+            name = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME_FAR)
+            address = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS_FAR)
 
-        result.update({"name": name, "address": address})
+            if is_lat_lng:
+                lat, lng = random.choice(_fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG_FAR)
+                result.update({"lat": lat, "lng": lng})
+
+            result.update({"name": name, "address": address})
+
+        else:
+            result = []
+
+            name_list = random.sample(
+                _fake_data.FakeRestaurantData.FAKE_RESTAURANT_NAME_FAR, number
+            )
+            address_list = random.sample(
+                _fake_data.FakeRestaurantData.FAKE_RESTAURANT_ADDRESS_FAR, number
+            )
+
+            for i in range(number):
+                tmp = {"name": name_list[i], "address": address_list[i]}
+
+                result.append(tmp)
+
+            if is_lat_lng:
+                lat_lng_list = random.sample(
+                    _fake_data.FakeRestaurantData.FAKE_RESTAURANT_LAT_LNG_FAR, number
+                )
+
+                for i in range(number):
+                    lat, lng = lat_lng_list[i]
+
+                    result[i].update({"lat": lat, "lng": lng})
 
         return result
 
-    def fake_current_location():
+    def fake_current_location(number: int = 1):
         """生成目標位置
 
         如果要測試在附近的餐廳，使用 `fake_restaurant()` ( 5KM 內 )
@@ -70,9 +122,13 @@ class FakeData:
 
         fake_location = [(24.94409, 121.22538), (24.94442, 121.22347), (24.94135, 121.22447)]
 
-        return random.choice(fake_location)
+        if number <= 1:
+            return random.choice(fake_location)
 
-    def fake_current_location_far():
+        else:
+            return random.sample(fake_location, number)
+
+    def fake_current_location_far(number: int = 1):
         """生成目標位置
 
         如果要測試在附近的餐廳，使用 `fake_restaurant_far()` ( 5KM 內 )
@@ -80,56 +136,137 @@ class FakeData:
 
         fake_location = [(22.98856, 120.23490), (22.98892, 120.23356), (22.98796, 120.23541)]
 
-        return random.choice(fake_location)
+        if number <= 1:
+            return random.choice(fake_location)
 
-    def fake_restaurant_open_time(to_str: bool = False):
+        else:
+            return random.sample(fake_location, number)
+
+    def fake_restaurant_open_time(to_str: bool = False, number: int = 1):
         """生成假餐廳營業時間
 
         ( 不會包括 restaurant_id 欄位，需要自己增加，因為考慮之後會要根據 restaurant 的資料來關聯)
 
         """
 
-        day_of_week = random.choice(_fake_data.FakeRestaurantOpenTimeData.DAY_OF_WEEK)
-        open_time = random.choice(_fake_data.FakeRestaurantOpenTimeData.OPEN_TIME)
-        close_time = random.choice(_fake_data.FakeRestaurantOpenTimeData.CLOSE_TIME)
+        if number <= 1:
+            day_of_week = random.choice(_fake_data.FakeRestaurantOpenTimeData.DAY_OF_WEEK)
+            open_time = random.choice(_fake_data.FakeRestaurantOpenTimeData.OPEN_TIME)
+            close_time = random.choice(_fake_data.FakeRestaurantOpenTimeData.CLOSE_TIME)
 
-        if to_str:
-            open_time = open_time.strftime("%H:%M")
-            close_time = close_time.strftime("%H:%M")
+            if to_str:
+                open_time = open_time.strftime("%H:%M")
+                close_time = close_time.strftime("%H:%M")
 
-        return {"day_of_week": day_of_week, "open_time": open_time, "close_time": close_time}
-
-    def fake_restaurant_type():
-        """生成假餐廳種類資料"""
-
-        name = random.choice(_fake_data.FakeRestaurantType.NAME)
-        desc = random.choice(_fake_data.FakeRestaurantType.DESC)
-
-        return {"name": name, "desc": desc}
-
-    def fake_user(is_oauth: bool = False):
-        data = {}
-
-        username = random.choice(_fake_data.FakeUser.USERNAME)
-        email = random.choice(_fake_data.FakeUser.EMAIL)
-
-        if not is_oauth:
-            password_hash = random.choice(_fake_data.FakeUser.PASSWORD_HASH)
-
-            data.update({"password_hash": password_hash})
+            result = {"day_of_week": day_of_week, "open_time": open_time, "close_time": close_time}
 
         else:
-            data.update({"is_oauth": True})
+            result = []
 
-        data.update({"username": username, "email": email})
+            day_of_week_list = random.sample(
+                _fake_data.FakeRestaurantOpenTimeData.DAY_OF_WEEK, number
+            )
+            open_time_list = random.sample(_fake_data.FakeRestaurantOpenTimeData.OPEN_TIME, number)
+            close_time_list = random.sample(
+                _fake_data.FakeRestaurantOpenTimeData.CLOSE_TIME, number
+            )
 
-        return data
+            for i in range(number):
+                open_time = open_time_list[i]
+                close_time = close_time_list[i]
 
-    def fake_oauth():
-        provider = random.choice(_fake_data.FakeOAuth.PROVIDER)
-        access_token = random.choice(_fake_data.FakeOAuth.ACCESS_TOKEN)
+                if to_str:
+                    open_time = open_time.strftime("%H:%M")
+                    close_time = close_time.strftime("%H:%M")
 
-        return {"provider": provider, "access_token": access_token}
+                tmp = {
+                    "day_of_week": day_of_week_list[i],
+                    "open_time": open_time,
+                    "close_time": close_time,
+                }
+
+                result.append(tmp)
+
+        return result
+
+    def fake_restaurant_type(number: int = 1):
+        """生成假餐廳種類資料"""
+
+        if number <= 1:
+            name = random.choice(_fake_data.FakeRestaurantType.NAME)
+            desc = random.choice(_fake_data.FakeRestaurantType.DESC)
+
+            result = {"name": name, "desc": desc}
+
+        else:
+            name_list = random.sample(_fake_data.FakeRestaurantType.NAME, number)
+            desc_list = random.sample(_fake_data.FakeRestaurantType.DESC, number)
+
+            result = []
+
+            for i in range(number):
+                tmp = {"name": name_list[i], "desc": desc_list[i]}
+
+                result.append(tmp)
+
+        return result
+
+    def fake_user(is_oauth: bool = False, number: int = 1):
+        if number <= 1:
+            username = random.choice(_fake_data.FakeUser.USERNAME)
+            email = random.choice(_fake_data.FakeUser.EMAIL)
+
+            result = {"username": username, "email": email}
+
+            if not is_oauth:
+                password_hash = random.choice(_fake_data.FakeUser.PASSWORD_HASH)
+
+                result.update({"password_hash": password_hash})
+
+            else:
+                result.update({"is_oauth": True})
+
+        else:
+            result = []
+
+            username_list = random.sample(_fake_data.FakeUser.USERNAME, number)
+            email_list = random.sample(_fake_data.FakeUser.USERNAME, number)
+
+            if not is_oauth:
+                password_hash_list = random.sample(_fake_data.FakeUser.PASSWORD_HASH, number)
+
+            for i in range(number):
+                tmp = {"username": username_list[i], "email": email_list[i]}
+
+                if not is_oauth:
+                    tmp.update({"password_hash": password_hash_list[i]})
+
+                else:
+                    tmp.update({"is_oauth": True})
+
+                result.append(tmp)
+
+        return result
+
+    def fake_oauth(number: int = 1):
+        if number <= 1:
+            provider = random.choice(_fake_data.FakeOAuth.PROVIDER)
+            access_token = random.choice(_fake_data.FakeOAuth.ACCESS_TOKEN)
+
+            result = {"provider": provider, "access_token": access_token}
+
+        else:
+            result = []
+
+            provider_list = random.sample(_fake_data.FakeOAuth.PROVIDER, number)
+            access_token_list = random.sample(_fake_data.FakeOAuth.ACCESS_TOKEN, number)
+
+            for i in range(number):
+                tmp = {"provider": provider_list[i], "access_token": access_token_list[i]}
+
+                result.append(tmp)
+
+        return result
 
 
 class FakeDataBase:
